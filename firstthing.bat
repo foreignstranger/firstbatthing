c382a3a7ee2b4b5ab5510feae353aba80d36b20b
foreignstranger <104476844+foreignstranger@users.noreply.github.com>
2022-04-26 23:39:57 -0300

    Add files via upload

---
+++ firstthing.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+ECHO hello stranger random person
+PAUSE
+ECHO wait why did you press that key?
+PAUSE
+ECHO please don't press any key, preson... i'll get out if you keep pressing keys
+PAUSE
+ECHO im gonna disappear from your screen if you keep doing it :0
+PAUSE
+ECHO no pls
+PAUSE
+ECHO i said NO
+PAUSE
+ECHO BRO WTF PLS ISTG NO AAAAAAAA
+PAUSE
+ECHO NONNOONONONONNO PLSPLPLPSLPLSPLPPSL
+PAUSE
+ECHO AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+PAUSE
+ECHO https://youtu.be/4DxBkW3r8pA
+PAUSE
