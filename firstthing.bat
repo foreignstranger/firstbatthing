064faceaadcaa446e88ef67704fa641b981ac7d5
foreignstranger <gogo10subs@gmail.com>
2022-04-27 00:47:37 -0300

    ok this is getting professional, i even added a variable and a condition!!!

--- firstthing.bat
+++ firstthing.bat
@@ -1,21 +1,56 @@
-@ECHO OFF
-ECHO hello stranger random person
-PAUSE
-ECHO wait why did you press that key?
-PAUSE
-ECHO please don't press any key, preson... i'll get out if you keep pressing keys
-PAUSE
-ECHO bro im gonna disappear from your screen if you keep doing it :0
-PAUSE
-ECHO no pls
-PAUSE
-ECHO i said NO!!!!
-PAUSE
-ECHO BRO WTF PLS ISTG NO AAAAAAAA
-PAUSE
-ECHO NONNOONONONONNO PLSPLPLPSLPLSPLPPSL
-PAUSE
-ECHO AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-PAUSE
-ECHO https://youtu.be/4DxBkW3r8pA
-PAUSE
+@echo off
+echo hello stranger random person
+pause
+
+echo wait why did you press that key?
+pause
+
+echo please don't press any key, preson... i'll get out if you keep pressing keys
+pause
+
+echo bro im gonna disappear from your screen if you keep doing it :0
+pause
+
+echo no pls
+pause
+
+echo i said NO!!!!
+pause
+
+echo BRO WTF PLS ISTG NO AAAAAAAA
+pause
+
+echo NONNOONONONONNO PLSPLPLPSLPLSPLPPSL
+pause
+
+echo AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+pause
+
+echo CAN YOU PLEASE JUST STOP?????? ;_; :sob::sob::sob::sob:
+
+:question
+set /p answer="[use y/n]"
+
+:: never do [variable == value]
+:: spaces count as a part of the variable name, bro
+:: always go like [variable==value]
+
+if "%answer%"=="y" (
+    echo oh god thank you :pray::pray::pray::pray:
+    pause
+) else (
+    if "%answer%"=="n" (
+        echo k then... it was good to be alive... while it lasted ._.
+        pause
+
+        echo just press any key to end my suffering, go ahead...
+        pause
+
+        echo https://youtu.be/4DxBkW3r8pA
+        pause
+    ) else (
+        echo wait this key is unknown :p
+        echo ...i'll ask once again... will... you... STOP?
+        goto question
+    )
+)
